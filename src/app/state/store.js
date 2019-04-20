6bd39aba4a9b76df8aebfe7c89a675a4a7db0005
David de Lusenet <david.delusenet@random.studio>
2019-04-20 08:46:31 +0200

    Add Thunk middleware

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import mainReducer from './reducers/mainReducer';
+import thunk from 'redux-thunk';
 
 export default createStore(
   mainReducer,
-  applyMiddleware(logger),
+  applyMiddleware(thunk, logger),
 );
